02cf7cffa72c9a96d4e8d459d2ce9560b522889e
luoxt <371754168@qq.com>
2018-05-13 23:10:00 +0800

    根系

--- down.go
+++ down.go
@@ -17,56 +17,38 @@ func init()  {
 	//加载配置文件
 	getConfig()
 
-	fmt.Println("dddd")
-
 }
 
 func main() {
 
+	command()
+
+	
+
+}
+
+//获取命令参数
+func command()  {
 	//获取用户输入的命令
 	commandstr := ""
 
 	args := os.Args
 	if args == nil || len(args)<2 || len(args[1])<2 {
-		commandstr = command()
+		//获取用户输入的
+		fmt.Println("请输入命令:")
+		firstInput := bufio.NewScanner(os.Stdin)
+
+		if firstInput.Scan() {
+			commandstr = firstInput.Text()
+		}
 	} else {
 		commandstr = args[1]
 	}
 
 	cmd.Command(commandstr)
-
 }
 
-//获取命令参数
-func command() string  {
-	//获取用户输入的
-	fmt.Println("请输入命令:")
-	firstInput := bufio.NewScanner(os.Stdin)
 
-	firstPath := ""
-	if firstInput.Scan() {
-		firstPath = firstInput.Text()
-	}
-	if len(firstPath) <2 {
-		return command()
-	} else {
-		return firstPath
-	}
-}
-
-type database struct {
-	host string
-	dbname string
-	user string
-	pass string
-	port string
-}
-
-type Config struct {
-	datebase database
-	path  string
-	gocount int
-}
 
 //获取配置文件
 func getConfig()  {
@@ -78,7 +60,7 @@ func getConfig()  {
 		path = "\\"
 	}
 
-	allConfigPath := dir + path + "src" + path + "go-imgdown" + path + "conf" + path + configFile
+	allConfigPath := dir + path + "conf" + path + configFile
 	fmt.Println(allConfigPath)
 
 	configbye, err := ioutil.ReadFile(allConfigPath)
@@ -86,19 +68,48 @@ func getConfig()  {
 		fmt.Print(err)
 	}
 
-	v := new(Config)
-	json.Unmarshal(configbye, &v)
-	fmt.Println(v.gocount)
-	//str := string(configbye)
-	//fmt.Println(str)
+	str := string(configbye)
+	fmt.Println(str)
+
+	//////////////////////////////////////////
+	type database struct {
+		host string
+		dbname string
+		user string
+		pass string
+		port string
+	}
+	
+	type Config struct {
+		datebases database 
+		path  string
+		gocount int
+	}
+
+	///////////////////////////////
+	type Book struct { 
+		Title string 
+		Authors []string 
+		Publisher string 
+		IsPublished bool 
+		Price float32
+	} 
+	
+	b := []byte(`{"Title": "Go语言编程", "Sales": 1000000}`) 
+	var gobook Book
+
+	err = json.Unmarshal(b, &gobook)
+    fmt.Println(gobook)
+
 	//
 	//inputFile, err := os.Open(allConfigPath)
 	//if err != nil {
 	//	fmt.Printf("请确认配置文件是否正确？")
 	//	return
 	//}
+
 	//defer inputFile.Close()
-	//
+
 	//inputReader := bufio.NewReader(inputFile)
 	//for {
 	//	inputString, err := inputReader.ReadString('\n')
